75fcffb6aa966098855929af0e7fa6e850bedd07
Lucas-Melo0 <lucassss8v@gmail.com>
2022-11-01 09:05:22 -0300

    feat: Problem without interfaces

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	/* sb := spanishBot{} */
+	printGreeting(eb)
+	/* printGreeting(sb) */
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+/* func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+} */
+
+func (eb englishBot) getGreeting() string {
+	// Custom logic
+	return "Hi there!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	// Custom logic
+	return "Hola"
 }
